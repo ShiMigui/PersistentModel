292adcda06a7fc896a5423a7e4431982043619fb
Miguel Nascimento dos Santos <miguel.nasto@gmail.com>
2024-11-22 02:37:38 -0300

    Adding hidden class, hide, and unhide functions

---
+++ docs/script/func/hidden.js
@@ -0,0 +1,19 @@
+const WORD = 'hidden';
+
+/**
+ * Hides the element by adding the `hidden` class.
+ * 
+ * @param {HTMLElement} el - The element to hide by adding the `hidden` class.
+ */
+export function hide(el) {
+    el.classList.add(WORD);
+}
+
+/**
+ * Shows the element by removing the `hidden` class.
+ * 
+ * @param {HTMLElement} el - The element to show by removing the `hidden` class.
+ */
+export function unhide(el) {
+    el.classList.remove(WORD);
+}
